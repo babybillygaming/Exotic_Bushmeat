a3a765dfa85405dc010b3870c8a4308b03cfd95b
Baby Billy Gaming <neil@bluetonic.org>
2023-07-07 20:06:04 -0500

    More fixes

--- src/main/java/com/greb/bushmeat/item/ModFoods.java
+++ src/main/java/com/greb/bushmeat/item/ModFoods.java
@@ -3,7 +3,7 @@ package com.greb.bushmeat.item;
 import net.minecraft.world.food.FoodProperties;
 
 public class ModFoods {
-    public static final FoodProperties AXOLOTL_LEG = (new FoodProperties.Builder()).nutrition(4).saturationMod(0.2F).build();
+    public static final FoodProperties AXOLOTL_LEG = (new FoodProperties.Builder()).nutrition(2).saturationMod(0.2F).build();
     public static final FoodProperties COOKED_AXOLOTL_LEG = (new FoodProperties.Builder()).nutrition(6).saturationMod(0.6F).build();
-    public static final FoodProperties FRIED_AXOLOTL_LEG = (new FoodProperties.Builder()).nutrition(10).saturationMod(1.0F).build();
+    public static final FoodProperties FRIED_AXOLOTL_LEG = (new FoodProperties.Builder()).nutrition(12).saturationMod(1.2F).build();
 }
--- src/main/java/com/greb/bushmeat/item/ModItems.java
+++ src/main/java/com/greb/bushmeat/item/ModItems.java
@@ -11,7 +11,7 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, bushmeat.MOD_ID);
-    public static final RegistryObject<Item> AXOLOTL_LEG = ITEMS.register("axolotl_leg", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(ModFoods.COOKED_AXOLOTL_LEG)));
+    public static final RegistryObject<Item> AXOLOTL_LEG = ITEMS.register("axolotl_leg", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(ModFoods.AXOLOTL_LEG)));
     public static final RegistryObject<Item> COOKED_AXOLOTL_LEG = ITEMS.register("cooked_axolotl_leg", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(ModFoods.COOKED_AXOLOTL_LEG)));
     public static final RegistryObject<Item> FRIED_AXOLOTL_LEG = ITEMS.register("fried_axolotl_leg", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(ModFoods.FRIED_AXOLOTL_LEG)));
     public static void register(IEventBus eventBus) {
